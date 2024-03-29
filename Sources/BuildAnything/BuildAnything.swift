e64e76b8df5c26274ad93e65bb2ac5fe1faba3d6
Alex Nagy <rebeloperalpar@gmail.com>
2024-03-29 13:21:14 +0200

    initial commit

--- Sources/BuildAnything/BuildAnything.swift
+++ Sources/BuildAnything/BuildAnything.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct BuildAnything {
+    public func launch(with apiKey: String) {
+        print("Successfully launched BuildAnything with API Key: \(apiKey)")
+    }
+}
